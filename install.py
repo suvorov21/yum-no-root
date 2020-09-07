887b22d6f610513548904cb993d4f18d3bece835
Sergey SUVOROV <suvorov@inr.ru>
2020-09-07 09:13:21 +0200

    add CLI

--- install.py
+++ install.py
@@ -1,6 +1,8 @@
 import subprocess
 import os
 
+import click
+
 RPM_PATH = '/t2k/users/suvorov/rpm_v2/'
 PREFIX = '/t2k/users/suvorov/centos_v2/'
 
@@ -21,27 +23,28 @@ def resolve(packages):
 
     return packages
 
-def download(packages):
+def download(packages, rpm_path):
     """Download rpms."""
+    os.chdir(rpm_path)
     for pack in packages:
         result = subprocess.run(['yumdownloader', '--resolve', pack],
                                 check=True, stdout=subprocess.PIPE
                                )
         print(result.stdout.decode("utf-8"))
 
-def install(run=False):
+def install(rpm_path, prefix, run=False):
     """Install the rpms to the PREFIX."""
-    os.chdir(PREFIX)
+    os.chdir(prefix)
     subprocess.run(['mkdir', '-p', 'tmp_install'], check=False)
     with open('install.sh', 'w') as w_file:
         w_file.write('cd tmp_install\n')
-        for file in os.listdir(RPM_PATH):
+        for file in os.listdir(rpm_path):
             if file.endswith('.rpm'):
                 w_file.write(f'echo "Working on {file}"\n')
-                w_file.write(f'rpm2cpio {RPM_PATH+file} | cpio -idmv\n')
+                w_file.write(f'rpm2cpio {rpm_path+file} | cpio -idmv\n')
                 w_file.write('chmod -R 765 ./*\n')
-                w_file.write(f'cp -r {PREFIX}/tmp_install/* {PREFIX}\n')
-                w_file.write(f'rm -rf {PREFIX}/tmp_install/*\n')
+                w_file.write(f'cp -r {prefix}/tmp_install/* {prefix}\n')
+                w_file.write(f'rm -rf {prefix}/tmp_install/*\n')
     if run:
         res = subprocess.Popen("source install.sh",
                                shell=True,
@@ -49,31 +52,35 @@ def install(run=False):
                                )
         res.wait()
 
-def create_enable():
+def create_enable(prefix):
     """Create centOS installation script."""
-    with open(PREFIX + 'enable.sh', 'w') as file:
-        file.write(f'export PATH="{PREFIX}/usr/sbin:{PREFIX}/usr/bin:{PREFIX}/sbin:{PREFIX}/bin:$PATH"\n')
-        file.write(f'export LD_LIBRARY_PATH="{PREFIX}/usr/lib64:{PREFIX}/lib64"')
+    with open(prefix + 'enable.sh', 'w') as file:
+        file.write(f'export PATH="{prefix}/usr/sbin:{prefix}/usr/bin:{prefix}/sbin:{prefix}/bin:$PATH"\n')
+        file.write(f'export LD_LIBRARY_PATH="{prefix}/usr/lib64:{prefix}/lib64"')
 
-def cast_enable():
+def cast_enable(prefix):
     """Cast paths in enable paths from absolute to relative."""
-    for root, dirs, files in os.walk(os.path.join(PREFIX, "opt/")):
+    for root, dirs, files in os.walk(os.path.join(prefix, "opt/")):
         for file in files:
             if file == 'enable':
                 print(root, dirs, file)
                 with open(os.path.join(root, file), 'r+') as file_w:
                     data = file_w.read()
                     data = data.replace('=/opt/rh',
-                                        f'={PREFIX}/opt/rh/'
+                                        f'={prefix}/opt/rh/'
                                         )
                     data = data.replace(':/opt/rh',
-                                        f':{PREFIX}/opt/rh/'
+                                        f':{prefix}/opt/rh/'
                                         )
                     file_w.seek(0)
                     file_w.write(data)
                     file_w.truncate()
 
-if __name__ == "__main__":
+@click.command()
+@click.option('--prefix', default='~/')
+@click.option('--rpm_path', default='~/')
+def main(prefix, rpm_path):
+    """Main sequence."""
     packages_dev_latest = {'rh-python38-python-devel.x86_64',
                            'devtoolset-9.x86_64'
                            }
@@ -98,7 +105,10 @@ if __name__ == "__main__":
 
     packages_ini = packages_devgroup | packages_t2k | packages_dev_latest
     packs = resolve(packages_ini)
-    download(packs)
-    install(True)
-    create_enable()
-    cast_enable()
+    download(packs, rpm_path)
+    install(rpm_path, prefix, True)
+    create_enable(prefix)
+    cast_enable(prefix)
+
+if __name__ == "__main__":
+    main()
